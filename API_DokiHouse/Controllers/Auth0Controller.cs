dc655dd4ae6d7307389afe9ff851dd5212798f7c
8b477 <jonathan_buchet@outlook.fr>
2023-12-21 14:46:03 +0100

    mise à jour du readme

--- API_DokiHouse/Controllers/Auth0Controller.cs
+++ API_DokiHouse/Controllers/Auth0Controller.cs
@@ -7,12 +7,61 @@ namespace API_DokiHouse.Controllers
     [ApiController]
     public class Auth0Controller : ControllerBase
     {
+
+
+        private readonly ILogger<Auth0Controller> _logger;
+
+        public Auth0Controller(ILogger<Auth0Controller> logger) => (_logger) = (logger);
+
         [AllowAnonymous]
         [HttpGet]
         public string GetToken()
         {
-            return "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9BdFdmUlcyX2JOOHY5RjFVUk5FNSJ9.eyJpc3MiOiJodHRwczovL2Rldi1xcTdzMmo0cjB6enJ1a204LnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJFVk5EUmUzTHpRNzhUblppclRlN3JjRG1zdFpIMnVXMEBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9Eb2tpSG91c2UtQmFjayIsImlhdCI6MTcwMzEwNTMyOCwiZXhwIjoxNzAzMTkxNzI4LCJhenAiOiJFVk5EUmUzTHpRNzhUblppclRlN3JjRG1zdFpIMnVXMCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.Ap2-r3XXAuNwhzp22zo6odQFGnYNkIEcMYhqSlcKwPMYQlNpxMGFsH63gonUYJfIxqUpyJmwNQE6pRnkMPwEtszaqR1vJO6xWw6plN1GxTn8vySKKsgBYH-ma8yr01DHupu3-yh0WNtgsmGawkwf-9Pn2QqaF1ld1jWOPP8cqA5PL8VNPMvCN9nyCeSYqwBOK17smsO97D9O5JIWc5F1bsI6h7BHTsna3tdmWYTCs9gVQMyIwZ2cOZZYuU97Ce6HDGZ0-SHN7pcJ52ei9clCqcB71qlynmCLb5ExrUttwQ_cROkxC9EV5_dqmDMlnTLgUITPBe5m6itLdyKiilAWkA";
+            return "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9BdFdmUlcyX2JOOHY5RjFVUk5FNSJ9.eyJpc3MiOiJodHRwczovL2Rldi1xcTdzMmo0cjB6enJ1a204LnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJFVk5EUmUzTHpRNzhUblppclRlN3JjRG1zdFpIMnVXMEBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9Eb2tpSG91c2UtQmFjayIsImlhdCI6MTcwMzE1NTc4MCwiZXhwIjoxNzAzMjQyMTgwLCJhenAiOiJFVk5EUmUzTHpRNzhUblppclRlN3JjRG1zdFpIMnVXMCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.UaRAGjcmvoN_FCjyMTY3WZuUyBUrfDYj6f1Utw80zsyPwdndQIwlvxGnhBLJB115CcOE9EkTTTl-SdkmxJxhWkO2bD_KtljotgSVeUrALgo4iePikQsibhtUsljNU4JX3c4USgiiQbkbG4rlFOxod2Yn2tuZ6MSGp66VpfBiM9B5rWk7rqzImJ6P8117mHyAhEFFNlj9Hjybrxr-w-MytHW5ELX2mjsvKCv1T3qiUGH6PFpwGEGT34qDPBSa10BVFHKOnTcfMPp5cs5-hFkE_SdXyqQRF7zpdk1T07xUWSeavxIZlbiUQAe-SVQe2QvKiMjIEPeS8LbXsshDA1UM4Q";
         }
 
+        /// <summary>
+        /// Démo comment mettre en place la possibilité d'annulé une requête sur une méthode asynchrone
+        /// </summary>
+        /// <param name="cancel"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpGet(nameof(AnnulationDeRequeteSurUneMethodeAsynchrone))]
+        public async Task<string> AnnulationDeRequeteSurUneMethodeAsynchrone(CancellationToken cancel)
+        {
+
+            _logger.LogWarning("Début de la demande !!!!!!!");
+
+            await Task.Delay(3000,cancel);
+
+            _logger.LogWarning("fin de l'attente");
+
+            return "finito";
+        }
+
+
+        /// <summary>
+        /// Démo comment mettre en place la possibilité d'annulé une requête sur une méthode synchrone
+        /// </summary>
+        /// <param name="cancel"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpGet(nameof(AnnulationDeRequeteSurUneMethodeSynchrone))]
+        public async Task<string> AnnulationDeRequeteSurUneMethodeSynchrone(CancellationToken cancel)
+        {
+
+            _logger.LogWarning("Début de la demande !!!!!!!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                _logger.LogWarning("Dans la boucle i = " + i);
+                cancel.ThrowIfCancellationRequested();
+                Thread.Sleep(2000);
+            }
+            
+            _logger.LogWarning("fin de l'attente");
+
+            return "finito";
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# API_DokiHouse
+# API_DokiHouse  
+
+Création d'une API sur la gestion d'un Bonsaï :  
+
+- Création d'un profil perso (User).
+- Espace gestion Bonsaï, ajout, suivis, notification.
+
+
+But à terme de l'api et du front 
+- Mise en relation des utilisateurs autour d'une même passion.
+- Possibilité pour l'utilisateur de publier des posts ou de commenter.
