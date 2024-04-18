99f01253f534538914edce4c8f936dd444a0e76f
8b477 <jonathan_buchet@outlook.fr>
2024-04-18 17:46:15 +0200

    rename models updateMail => field Value as now Mail

--- BLL_DokiHouse/Models/User/UserCheckActualMail.cs
+++ BLL_DokiHouse/Models/User/UserCheckActualMail.cs
@@ -5,9 +5,9 @@ namespace BLL_DokiHouse.Models.User
 {
     public class UserCheckActualMail
     {
-        [Required(ErrorMessage = $"{nameof(Value)} : champ requis")]
-        [DataType(DataType.EmailAddress, ErrorMessage = $"{nameof(Value)} : champ requis, {nameof(Value)} : non valide !")]
-        [EmailAddress(ErrorMessage = $"{nameof(Value)} : champ requis, {nameof(Value)} : non valide !")]
-        public string Value { get; set; } = string.Empty;
+        [Required(ErrorMessage = $"{nameof(Mail)} : champ requis")]
+        [DataType(DataType.EmailAddress, ErrorMessage = $"{nameof(Mail)} : champ requis, {nameof(Mail)} : non valide !")]
+        [EmailAddress(ErrorMessage = $"{nameof(Mail)} : champ requis, {nameof(Mail)} : non valide !")]
+        public string Mail { get; set; } = string.Empty;
     }
 }
